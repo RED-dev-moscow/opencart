1c63fac0a387eda812ab64c27cb93c607bc1fcd0
Harold Finch <whistler@exec.id-chain.org>
2019-10-14 05:50:39 +0300

    master update

--- master_file.php
+++ master_file.php
@@ -3188,14 +3188,3 @@ $_['text_checkout']    = 'Checkout';
 $_['text_search']      = 'Search';
 $_['text_information'] = 'Information';
 $_['text_contact']     = 'Contact Us';<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
-<head>
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<title>Untitled Document</title>
-</head>
-
-<body>
-</body>
-</html>
-// Text
-$_['text_error'] = 'Information Page Not Found!';
